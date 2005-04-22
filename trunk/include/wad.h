ada9479c19c75ded234482054b59adec0b42d4dc
saschamueller <saschamueller@93ce3444-17f4-0310-869f-feeeb9b2a701>
2005-04-22 13:23:50 +0000

    Added GPL notice to all source files and renamed doc/+uds to doc/uds

--- trunk/include/wad.h
+++ trunk/include/wad.h
@@ -1,3 +1,14 @@
+// Godafude - A Map Editor for DOOM 
+// Copyright (c) 2005 Sascha Mueller <dev@saschamueller.com>
+
+/******************************************************************
+ * This program is free software; you can redistribute it and/or  *
+ * modify it under the terms of the GNU General Public License    *
+ * as published by the Free Software Foundation; either version 2 *
+ * of the License, or (at your option) any later version.         *
+ ******************************************************************/
+
+
 #ifndef WAD_H
 #define WAD_H
 
--- trunk/src/mapview.cpp
+++ trunk/src/mapview.cpp
@@ -1,3 +1,13 @@
+// Godafude - A Map Editor for DOOM 
+// Copyright (c) 2005 Sascha Mueller <dev@saschamueller.com>
+
+/******************************************************************
+ * This program is free software; you can redistribute it and/or  *
+ * modify it under the terms of the GNU General Public License    *
+ * as published by the Free Software Foundation; either version 2 *
+ * of the License, or (at your option) any later version.         *
+ ******************************************************************/
+
 #include <QAction>
 #include <QActionGroup>
 #include <QPaintEvent>
